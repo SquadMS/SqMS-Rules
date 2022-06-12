39cf22e8cb2dcf27e316766c704260cd9a14c9c2
Patrick <privat@skyraptor.eu>
2022-06-12 04:25:27 +0200

    Create RuleArticleResource.php

---
+++ src/Filament/Resources/RuleArticleResource.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace SquadMS\AdminConfig\Filament\Resources;
+
+use SquadMS\Rules\Filament\Resources\AdminConfigResource\Pages;
+use SquadMS\Rules\Models\RuleArticle;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+
+class AdminConfigResource extends Resource
+{
+    protected static ?string $navigationGroup = 'Rules Management';
+
+    protected static ?string $model = RuleArticle::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->rules('required|string|min:1|max:255')
+                    ->required(),
+                Forms\Components\TextInput::make('content')
+                    ->nullable(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')->sortable(),
+            ])
+            ->filters([
+                //
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListRuleArtciles::route('/'),
+            'create' => Pages\CreateRuleArtciles::route('/create'),
+            'edit' => Pages\EditRuleArtciles::route('/{record}/edit'),
+        ];
+    }
+}
