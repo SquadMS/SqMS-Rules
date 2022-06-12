04285c1e29112405f917633cef7e1a290fdd26bf
Patrick <privat@skyraptor.eu>
2022-06-12 04:27:49 +0200

    Create EditRuleArticle.php

---
+++ src/Filament/Resources/RuleArticleResource/Pages/EditRuleArticle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace SquadMS\Rules\Filament\Resources\RuleArticleResource\Pages;
+
+use SquadMS\Rules\Filament\Resources\RuleArticleResource;
+use Filament\Pages\Actions\Action;
+use Filament\Resources\Pages\EditRecord;
+
+class EditRuleArticle extends EditRecord
+{
+    protected static string $resource = RuleArticleResource::class;
+}
