a5e7a91113c323a5ab1e491faefc1b12e4a16bd5
Patrick <privat@skyraptor.eu>
2022-06-12 11:37:50 +0200

    Update RuleArticle.php

--- src/Models/RuleArticle.php
+++ src/Models/RuleArticle.php
@@ -4,10 +4,12 @@ namespace SquadMS\Rules\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Translatable\HasTranslations;
+use Spatie\EloquentSortable\Sortable;
+use Spatie\EloquentSortable\SortableTrait;
 
-class RuleArticle extends Model
+class RuleArticle extends Model implements Sortable
 {
-    use HasTranslations;
+    use HasTranslations, SortableTrait;
 
     /**
      * The attributes that are mass assignable.
