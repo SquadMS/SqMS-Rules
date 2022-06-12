6a369e6d135a65d66c002e066fb7fbf67e945948
Patrick <privat@skyraptor.eu>
2022-06-12 04:36:26 +0200

    Update SquadMSModule.php

--- src/SquadMSModule.php
+++ src/SquadMSModule.php
@@ -1,17 +1,8 @@
-
-
-
 <?php
 
 namespace SquadMS\AdminConfig;
 
 use Illuminate\Console\Scheduling\Schedule;
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\View;
-use SquadMS\Foundation\Facades\SquadMSAdminMenu;
-use SquadMS\Foundation\Facades\SquadMSMenu;
-use SquadMS\Foundation\Helpers\NavigationHelper;
-use SquadMS\Foundation\Menu\SquadMSMenuEntry;
 use SquadMS\Foundation\Modularity\Contracts\SquadMSModule as SquadMSModuleContract;
 
 
