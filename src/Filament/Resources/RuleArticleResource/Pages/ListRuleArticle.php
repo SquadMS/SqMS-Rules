4e5653c40627386517372025f1bcef77b083ad49
Patrick <privat@skyraptor.eu>
2022-06-12 04:28:43 +0200

    Create ListRuleArticle.php

---
+++ src/Filament/Resources/RuleArticleResource/Pages/ListRuleArticle.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SquadMS\Rules\Filament\Resources\RuleArticleResource\Pages;
+
+use SquadMS\Rules\Filament\Resources\RuleArticleResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListRuleArticle extends ListRecords
+{
+    protected static string $resource = RuleArticleResource::class;
+}
