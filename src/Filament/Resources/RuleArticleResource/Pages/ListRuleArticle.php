88faf328e022de654c5a87b7b6f8710319bcd9d2
Patrick <privat@skyraptor.eu>
2022-06-12 05:14:41 +0200

    Update ListRuleArticle.php

--- src/Filament/Resources/RuleArticleResource/Pages/ListRuleArticle.php
+++ src/Filament/Resources/RuleArticleResource/Pages/ListRuleArticle.php
@@ -4,8 +4,11 @@ namespace SquadMS\Rules\Filament\Resources\RuleArticleResource\Pages;
 
 use SquadMS\Rules\Filament\Resources\RuleArticleResource;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Pages\ListRecords\Concerns\Translatable;
 
 class ListRuleArticle extends ListRecords
 {
+    use Translatable;
+    
     protected static string $resource = RuleArticleResource::class;
 }
