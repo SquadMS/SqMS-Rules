40659db614f2db408e39d98c0b6863be6d51dc2a
Patrick <privat@skyraptor.eu>
2022-06-12 04:26:37 +0200

    Create CreateRuleArticle.php

---
+++ src/Filament/Resources/RuleArticleResource/Pages/CreateRuleArticle.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SquadMS\AdminConfig\Filament\Resources\RuleArticleResource\Pages;
+
+use SquadMS\Rules\Filament\Resources\RuleArticleResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateRuleArticle extends CreateRecord
+{
+    protected static string $resource = RuleArticleResource::class;
+}
