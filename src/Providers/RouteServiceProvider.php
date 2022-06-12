2ee9a8c6f4c7c58b4489d6844e026678563440d0
Patrick <privat@skyraptor.eu>
2022-06-12 04:21:04 +0200

    Create RouteServiceProvider.php

---
+++ src/Providers/RouteServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace SquadMS\AdminConfig\Providers;
+
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        /* Routes */
+        $routesPath = __DIR__.'/../../routes';
+
+        /* WEB routes */
+        Route::group([
+            'middleware' => ['web'],
+        ], function () use ($routesPath) {
+            $this->loadRoutesFrom($routesPath.'/web.php');
+        });
+
+        /* API routes */
+        Route::group([
+            'prefix'     => 'api',
+            'middleware' => 'api',
+        ], function () use ($routesPath) {
+            $this->loadRoutesFrom($routesPath.'/api.php');
+        });
+    }
+}
