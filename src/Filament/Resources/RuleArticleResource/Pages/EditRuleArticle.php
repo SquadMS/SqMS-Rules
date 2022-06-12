570b68b65fcee3d6dfe80bb816fa74f314016ce3
Patrick <privat@skyraptor.eu>
2022-06-12 05:13:15 +0200

    Update EditRuleArticle.php

--- src/Filament/Resources/RuleArticleResource/Pages/EditRuleArticle.php
+++ src/Filament/Resources/RuleArticleResource/Pages/EditRuleArticle.php
@@ -5,8 +5,11 @@ namespace SquadMS\Rules\Filament\Resources\RuleArticleResource\Pages;
 use SquadMS\Rules\Filament\Resources\RuleArticleResource;
 use Filament\Pages\Actions\Action;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Resources\Pages\EditRecord\Concerns\Translatable;
 
 class EditRuleArticle extends EditRecord
 {
+    use Translatable;
+    
     protected static string $resource = RuleArticleResource::class;
 }
