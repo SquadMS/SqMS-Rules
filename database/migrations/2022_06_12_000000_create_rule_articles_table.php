d0a1a4f41081195717084aea86134381074727ce
Patrick <privat@skyraptor.eu>
2022-06-12 03:54:23 +0200

    Create 2022_06_12_000000_create_rule_articles_table.php

---
+++ database/migrations/2022_06_12_000000_create_rule_articles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rule_articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->text('title');
+            $table->text('content');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rule_articles');
+    }
+};
