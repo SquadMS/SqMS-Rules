d856683b1f386e410d74ffb5256274e932d871c8
Patrick <privat@skyraptor.eu>
2022-06-12 05:13:49 +0200

    Update CreateRuleArticle.php

--- src/Filament/Resources/RuleArticleResource/Pages/CreateRuleArticle.php
+++ src/Filament/Resources/RuleArticleResource/Pages/CreateRuleArticle.php
@@ -4,8 +4,11 @@ namespace SquadMS\Rules\Filament\Resources\RuleArticleResource\Pages;
 
 use SquadMS\Rules\Filament\Resources\RuleArticleResource;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Resources\Pages\CreateRecord\Concerns\Translatable;
 
 class CreateRuleArticle extends CreateRecord
 {
+    use Translatable;
+    
     protected static string $resource = RuleArticleResource::class;
 }
