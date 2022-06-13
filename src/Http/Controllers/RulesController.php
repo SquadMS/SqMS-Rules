ea4f3c305eb47bb3799f2dc7fbf7a4521a25753a
Patrick <privat@skyraptor.eu>
2022-06-13 19:23:54 +0200

    Create RulesController.php

---
+++ src/Http/Controllers/RulesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace SquadMS\Rules\Http\Controllers;
+
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Config;
+
+class RulesController extends Controller
+{
+    /**
+     * Shows the rules page.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show()
+    {
+        /* Show home page */
+        return view(Config::get('sqms.theme').'::pages.rules');
+    }
+}
