c0dae65e87d845cf74e7187a6c1860ca0bce7f98
Patrick <privat@skyraptor.eu>
2022-06-13 19:22:43 +0200

    Update sqms-rules.php

--- config/sqms-rules.php
+++ config/sqms-rules.php
@@ -1,10 +1,25 @@
 <?php
 
 return [
-  'permissions' => [
-    'module' => 'sqms-rules',
-    'definitions' => [
-      'admin rules' => 'Grant access to the Rules management',
+  'routes' => [
+        'prefix'     => null,
+        'middleware' => ['web'],
+        'def'        => [
+            'rules' => [
+                'type'        => 'get',
+                'name'        => 'rules',
+                'path'        => '/rules',
+                'middlewares' => [],
+                'controller'  => \SquadMS\Rules\Http\Controllers\RulesController::class,
+                'executor'    => 'show',
+                'localized'   => true,
+            ],
+        ],
     ],
-  ]
+    'permissions' => [
+        'module' => 'sqms-rules',
+        'definitions' => [
+            'admin rules' => 'Grant access to the Rules management',
+        ],
+    ]
 ];
