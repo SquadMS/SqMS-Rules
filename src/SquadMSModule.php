c901c8fde3de3be028a4112efcc52e5e62c405b1
Patrick <privat@skyraptor.eu>
2022-06-13 02:09:14 +0200

    Update SquadMSModule.php

--- src/SquadMSModule.php
+++ src/SquadMSModule.php
@@ -16,11 +16,4 @@ class SquadMSModule extends SquadMSModuleContract
     {
         return 'SquadMS Rules';
     }
-    
-    public static function overrideConfigs(): array
-    {
-        return [
-            'filament-spatie-laravel-translatable-plugin.default_locales' => Config::get('sqms.locales'),
-        ];
-    }
 }
