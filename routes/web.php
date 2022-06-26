b44717a54520a307ae043d36cdcf399dd30bdd14
Patrick <privat@skyraptor.eu>
2022-06-26 11:57:44 +0200

    Fix: Routes not registering

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,9 @@ use Illuminate\Support\Facades\Route;
 use SquadMS\Foundation\Helpers\SquadMSRouteHelper;
 use SquadMS\Rules\Http\Controllers\RulesController;
 
-Route::middleware(['web'], function () {
+Route::group([
+    'middleware' => ['web']
+], function () {
     SquadMSRouteHelper::localized(function () {
         Route::get('rules', [RulesController::class, 'show'])->name('rules');
     });    
